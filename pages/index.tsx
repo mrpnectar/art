98728eff9b8a8653959c103bec67b9d449331607
MrP <125105940+mrpnectar@users.noreply.github.com>
2023-11-11 20:56:48 -0600

    Commit from Stub AI (#1)
    Co-authored-by: mrpnectar <bot@codestub.ai>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import BlockStacker from '../components/BlockStacker';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
+      <BlockStacker />
     </main>
-  )
+  );
 }
